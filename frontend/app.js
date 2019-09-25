24c431f7f21c69fa5d1d5c17391ef1a58be1616f
tanvir-interconnectionbd <tanvirislam@interconnectionbd.com>
2019-09-25 12:29:10 +0600

    Update patch request done here

--- frontend/app.js
+++ frontend/app.js
@@ -13,7 +13,33 @@ app.service("passDataService", function($rootScope){
   }
 });
 
-app.controller('employeeAddCtrl', function($scope, $http, passDataService, passDataService) {
+app.service("editDataService", function($rootScope){
+  this.tempData = {};
+  this.mode= "";
+
+  this.setData = function (data) {
+    this.tempData = data;
+    $rootScope.$emit("editDataEvent");
+  }
+
+  this.getData = function () {
+    return this.tempData;
+  }
+});
+
+app.controller('employeeAddCtrl', function($scope, $rootScope, $http, passDataService, editDataService) {
+  $rootScope.$on("editDataEvent", function(){
+    const editData = editDataService.getData();
+    $scope.first_name = editData.first_name;
+    $scope.last_name = editData.last_name;
+    $scope.address = editData.address;
+    $scope.age = editData.age;
+    $scope.email = editData.email;
+    $scope.buttonText = "Edit";
+    $scope.modeText = "Edit";
+  })
+  $scope.buttonText = "Add";
+  $scope.modeText = "Add";
   $scope.submit = function () {
     var data = {
       first_name : $scope.first_name,
@@ -22,14 +48,25 @@ app.controller('employeeAddCtrl', function($scope, $http, passDataService, passD
       age: $scope.age,
       email: $scope.email
     };
-    $http.post("http://127.0.0.1:8000/api/v1/employee-list/", data)
-    .then(function(response) {
-      passDataService.setData(response.data);
-    });
+
+    if($scope.modeText==="Add") {
+      $http.post("http://127.0.0.1:8000/api/v1/employee-list/", data)
+      .then(function(response) {
+        passDataService.setData(response.data);
+      });
+    }
+
+    if($scope.modeText==="Edit") {
+      const editId = editDataService.getData().id;
+      $http.patch("http://127.0.0.1:8000/api/v1/employee-detail/"+editId+"/", data)
+      .then(function(response) {
+        console.log(response);
+      });
+    }
   }
 });
 
-app.controller('employeeListCtrl', function($scope, $http, $rootScope, passDataService) {
+app.controller('employeeListCtrl', function($scope, $http, $rootScope, passDataService, editDataService) {
   $rootScope.$on("dataPassEvent",function() {
     var prevData = $scope.employeeList;
     prevData.push(passDataService.getData());
@@ -46,4 +83,11 @@ app.controller('employeeListCtrl', function($scope, $http, $rootScope, passDataS
       $scope.employeeList = afterDeletedList;
       })
   }
+
+  $scope.edit = function($id) {
+    const obj = $scope.employeeList.find(x => x.id === $id);
+    editDataService.setData(obj);
+  }
+
+
 });
